82f50cdcb9274a38e7852ce014b4352462744553
grbos <gregor.boschung@rega-sense.ch>
2023-06-30 16:36:49 +0200

    change to azure api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiUrl : "https://localhost:7005/api",
+   // apiUrl : "https://localhost:7005/api",
+    apiUrl : "https://germanwhistwebpage20230630135503.azurewebsites.net/api",
 };
